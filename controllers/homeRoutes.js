c34efb7cad4f9e1675a9cb76a07cb9bc45b76180
Tim Scott <0timothy.scott0@gmail.com>
2021-03-27 20:38:25 -0400

    temp main and dashboard pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const { User } = require('../models');
+const { requireSession } = require('../middlewares/auth');
 
 router.get('/', async (req, res) => {
   const dbUserData = await User.findAll();
@@ -10,6 +11,20 @@ router.get('/', async (req, res) => {
   );
 
   res.render('home', { users });
+});
+
+router.get('/dashboard', requireSession, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.sessionPayload.userId);
+    if (!userData) {
+      throw Error('no user');
+    }
+    const user = userData.get({ plain: true });
+    delete user.password;
+    res.render('dashboard', { user });
+  } catch {
+    res.redirect('/login');
+  }
 });
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const authRoutes = require('./auth');
 const apiRoutes = require('./api');
 
+const { isSession } = require('../middlewares/auth');
+router.use(isSession);
+
 router.use('/', homeRoutes);
+router.use('/', authRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
