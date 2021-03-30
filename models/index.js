225c45fa3daba4f7f1be74456931acaddc71072c
Edward Huang <ejhuang.2015@gmail.com>
2021-03-30 09:24:32 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,11 +1,6 @@
-// Dependencies
-// =============================================================
 const User = require('./User');
 const Exhibit = require('./Exhibit');
 
-// Joins
-// =============================================================
-// A user can have many exhibits but an exhibit can only have one user
 User.hasMany(Exhibit, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
@@ -15,6 +10,4 @@ Exhibit.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-// Export
-// =============================================================
 module.exports = { User, Exhibit };
