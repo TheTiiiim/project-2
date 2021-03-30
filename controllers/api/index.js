7f34eec92d8be101b18a3bd7f1e48febc7d03d96
Edward Huang <ejhuang.2015@gmail.com>
2021-03-29 20:25:30 -0400

    Renamed gallery to shortstack

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 
 const userRoutes = require('./user');
 const authRoutes = require('./auth');
-const galleryRoutes = require('./gallery');
+const shortStackRoutes = require('./shortStack');
 const exhibitRoutes = require('./exhibit');
 
 const { isAuth } = require('../../middlewares/auth');
 router.use(isAuth);
 
 router.use('/user', userRoutes);
-router.use('/gallery', galleryRoutes);
+router.use('/shortStack', shortStackRoutes);
 router.use('/exhibit', exhibitRoutes);
 router.use('/', authRoutes);
 
